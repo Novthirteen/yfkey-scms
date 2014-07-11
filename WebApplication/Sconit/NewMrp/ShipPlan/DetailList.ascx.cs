707bf23144ef7b75e85ddf9ea9f46e50a58bfaf3
tanyongji <tanyongji@sconit.com>
2014-07-11 14:29:41 +0800

    123

--- WebApplication/Sconit/NewMrp/ShipPlan/DetailList.ascx.cs
+++ WebApplication/Sconit/NewMrp/ShipPlan/DetailList.ascx.cs
@@ -176,8 +176,16 @@ from  MRP_ShipPlanDet as det
 
         #region   trace
         IList<ShipPlanDetTrace> traceList = new List<ShipPlanDetTrace>();
+        //for (int i = 0; i < shipPlanDetList; i++)
+        //{
+            
+        //}
+        // var ctraceList = this.TheGenericMgr.FindAllWithCustomQuery<ShipPlanDetTrace>(string.Format(" select l from ShipPlanDetTrace as l where l.Type='{0}' and  l.UUID in ('{1}') ", this.rbType.SelectedValue, string.Join("','", shipPlanDetList.Select(d => d.UUID).Distinct().ToArray())));
+        // if (ctraceList != null && ctraceList.Count > 0)
+        // {
+        //     traceList.AddRange(ctraceList);
+        // }
         traceList = this.TheGenericMgr.FindAllWithCustomQuery<ShipPlanDetTrace>(string.Format(" select l from ShipPlanDetTrace as l where l.Type='{0}' and  l.UUID in ('{1}') ", this.rbType.SelectedValue, string.Join("','", shipPlanDetList.Select(d => d.UUID).Distinct().ToArray())));
-
         if (traceList!=null && traceList.Count > 0)
         {
             foreach (var sd in shipPlanDetList)
@@ -200,7 +208,7 @@ from  MRP_ShipPlanDet as det
 
         #region  orderQty
         IList<ShipPlanOpenOrder> shipPlanOpenOrderList = new List<ShipPlanOpenOrder>();
-        shipPlanOpenOrderList = this.TheGenericMgr.FindAllWithCustomQuery<ShipPlanOpenOrder>(string.Format(" select l from ShipPlanOpenOrder as l where Type='{0}'  l.UUID in ('{1}') ", this.rbType.SelectedValue, string.Join("','", shipPlanDetList.Select(d => d.UUID).Distinct().ToArray())));
+        shipPlanOpenOrderList = this.TheGenericMgr.FindAllWithCustomQuery<ShipPlanOpenOrder>(string.Format(" select l from ShipPlanOpenOrder as l where l.Type='{0}' and  l.UUID in ('{1}') ", this.rbType.SelectedValue, string.Join("','", shipPlanDetList.Select(d => d.UUID).Distinct().ToArray())));
         shipPlanOpenOrderList = shipPlanOpenOrderList == null ? new List<ShipPlanOpenOrder>() : shipPlanOpenOrderList;
         if (shipPlanOpenOrderList!=null && shipPlanOpenOrderList.Count > 0)
         {
