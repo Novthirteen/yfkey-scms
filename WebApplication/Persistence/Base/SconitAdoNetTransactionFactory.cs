34752cce37b31d70ce4630f815a69751cfa7d998
novthirteen <novthirteen@gmail.com>
2014-10-14 10:40:03 +0800

    1

---
+++ WebApplication/Persistence/Base/SconitAdoNetTransactionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using NHibernate;
+using NHibernate.Engine;
+using NHibernate.Transaction;
+
+namespace com.Sconit.Persistence
+{
+    public class SconitAdoNetTransactionFactory : ITransactionFactory
+    {
+        public ITransaction CreateTransaction(ISessionImplementor session)
+        {
+            return new SconitAdoTransaction(session);
+        }
+
+        public void Configure(IDictionary props)
+        {
+        }
+    }
+}
